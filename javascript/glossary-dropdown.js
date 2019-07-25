e3eb4fac75fc207ca299cca815d85ca0d277c582
AndrePascalWerthwein <andre.werthwein@gmx.de>
2019-07-25 22:08:36 +0200

    Structure, Layout, Animation, Content
    _ Optimization of Glossary-Layout and Structure
    _ Integration of a JavaScript-based Dropdown-Animation
    _ Addition of exemplary Data to the Glossary

---
+++ javascript/glossary-dropdown.js
@@ -0,0 +1,27 @@
+var GlossaryEntries = document.querySelectorAll('.glossary-entry');
+var DropdownToggle = "";
+
+function PageLoad() {
+  for (var x = 0; x < GlossaryEntries.length; x = x + 1) {
+    GlossaryEntries[x].style.maxHeight = 45.4 + "px";
+  }
+}
+
+// Herstellung des Seitenaufbaus
+PageLoad();
+
+for (var x = 0; x < GlossaryEntries.length; x = x + 1) {
+  GlossaryEntries[x].addEventListener('click', function(e) {
+    e.preventDefault();
+
+    var DropdownCurrent = this.dataset.entry;
+
+    if (DropdownToggle === DropdownCurrent) {
+      PageLoad();
+      DropdownToggle = "";
+    } else {
+      this.style.maxHeight = 1000 + "px";
+      DropdownToggle = DropdownCurrent;
+    }
+  });
+}
